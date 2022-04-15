9422ef2c9c22dd07f26135f85b3380bd17ab1312
yoneyama ryo <uryonym@outlook.com>
2022-04-15 23:58:36 +0900

    5-1-1 ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+Rails
+  .application
+  .routes
+  .draw do
+    namespace :staff do
+      root 'top#index'
+    end
+
+    namespace :admin do
+      root 'top#index'
+    end
+
+    namespace :customer do
+      root 'top#index'
+    end
+  end
