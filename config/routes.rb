b5feeb3f460a5512d18fe11c6bca450b7de45e04
yoneyama ryo <uryonym@outlook.com>
2022-04-16 21:48:32 +0900

    7-3-3 セッション用のルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails
   .draw do
     namespace :staff do
       root 'top#index'
+      get 'login' => 'sessions#new', :as => :login
+      post 'session' => 'sessions#create', :as => :session
+      delete 'session' => 'sessions#destroy'
     end
 
     namespace :admin do
