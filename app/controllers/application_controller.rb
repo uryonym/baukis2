9cbe091e4bf728396a5e3fe7d3e8b6cea8907afe
yoneyama ryo <uryonym@outlook.com>
2022-04-16 15:10:04 +0900

    6-4 404 Not Found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
-  class Forbidden < ActionController::ActionControllerError; end
-  class IpAddressRejected < ActionController::ActionControllerError; end
+  class Forbidden < ActionController::ActionControllerError
+  end
+  class IpAddressRejected < ActionController::ActionControllerError
+  end
 
   rescue_from StandardError, with: :rescue500
   rescue_from Forbidden, with: :rescue403
   rescue_from IpAddressRejected, with: :rescue403
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue404
 
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
@@ -18,7 +21,11 @@ class ApplicationController < ActionController::Base
 
   private def rescue403(e)
     @exception = e
-    render "errors/forbidden", status: 403
+    render 'errors/forbidden', status: 403
+  end
+
+  private def rescue404(e)
+    render 'errors/not_found', status: 404
   end
 
   private def rescue500(e)
