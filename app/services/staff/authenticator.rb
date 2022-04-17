ee935b01203db2a1a7d25114292c9db891a54fea
yoneyama ryo <uryonym@outlook.com>
2022-04-17 16:58:20 +0900

    8-3-4 アカウントが停止状態の時のフラッシュメッセージを表示

--- app/services/staff/authenticator.rb
+++ app/services/staff/authenticator.rb
@@ -4,8 +4,8 @@ class Staff::Authenticator
   end
 
   def authenticate(raw_password)
-    @staff_member && !@staff_member.suspended? &&
-      @staff_member.hashed_password && @staff_member.start_date <= Date.today &&
+    @staff_member && @staff_member.hashed_password &&
+      @staff_member.start_date <= Date.today &&
       (@staff_member.end_date.nil? || @staff_member.end_date > Date.today) &&
       BCrypt::Password.new(@staff_member.hashed_password) == raw_password
   end
