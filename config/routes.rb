a40c6cc18750f3e5cd8ce06efc4e0cf87f297e04
yoneyama ryo <uryonym@outlook.com>
2022-04-18 23:45:41 +0900

    9-3 単数リソース

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,14 @@ Rails
     namespace :staff, path: '' do
       root 'top#index'
       get 'login' => 'sessions#new', :as => :login
-      post 'session' => 'sessions#create', :as => :session
-      delete 'session' => 'sessions#destroy'
+      resource :session, only: %i[create destroy]
+      resouce :account, except: %i[new create destroy]
     end
 
     namespace :admin do
       root 'top#index'
       get 'login' => 'sessions#new', :as => :login
-      post 'session' => 'sessions#create', :as => :session
-      delete 'session' => 'sessions#destroy'
-
+      resource :session, only: %i[create destroy]
       resources :staff_members
     end
 
