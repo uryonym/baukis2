6fc102bfa3379f488deaec333cffde1e24078563
yoneyama ryo <uryonym@outlook.com>
2022-04-19 00:03:08 +0900

    9-4 制約ルーティングの実装

---
+++ config/initializers/baukis2.rb
@@ -0,0 +1,16 @@
+Rails.application.configure do
+  config.baukis2 = {
+    staff: {
+      host: 'baukis2.example.com',
+      path: '',
+    },
+    admin: {
+      host: 'baukis2.example.com',
+      path: 'admin',
+    },
+    customer: {
+      host: 'example.com',
+      path: 'mypage',
+    },
+  }
+end
