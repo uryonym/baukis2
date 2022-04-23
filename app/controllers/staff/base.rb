776782682421ba569ba2315dbfcd5fde12af40e3
yoneyama ryo <uryonym@outlook.com>
2022-04-24 01:33:26 +0900

    12-1-4 職員ページのアクセス制御

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,4 +1,6 @@
 class Staff::Base < ApplicationController
+  before_action :authorize
+
   private def current_staff_member
     if session[:staff_member_id]
       @current_staff_member ||=
@@ -7,4 +9,11 @@ class Staff::Base < ApplicationController
   end
 
   helper_method :current_staff_member
+
+  private def authorize
+    unless current_staff_member
+      flash.notice = '職員としてろぐいんしてください。'
+      redirect_to :staff_login
+    end
+  end
 end
