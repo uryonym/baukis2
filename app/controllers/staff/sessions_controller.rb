bd9b5b0891a6ccad97f6e29555a4b4de97391a45
yoneyama ryo <uryonym@outlook.com>
2022-04-23 21:52:55 +0900

    11-2-1 アカウント管理機能のshowアクション

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -9,7 +9,7 @@ class Staff::SessionsController < Staff::Base
   end
 
   def create
-    @form = Staff::LoginForm.new(params[:staff_login_form])
+    @form = Staff::LoginForm.new(login_form_params)
     if @form.email.present?
       staff_member =
         StaffMember.find_by('LOWER(email) = ?', @form.email.downcase)
