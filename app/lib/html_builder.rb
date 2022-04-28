41165f7163c5f33c207e61c750ff4cf0d0d1785f
yoneyama ryo <uryonym@outlook.com>
2022-04-28 22:15:29 +0900

    15-2 HtmlBuilderの導入

---
+++ app/lib/html_builder.rb
@@ -0,0 +1,13 @@
+module HtmlBuilder
+  def markup(tag_name = nil, options = {})
+    root = Nokogiri::HTML::DocumentFragment.parse('')
+    Nokogiri::HTML::Builder.with(root) do |doc|
+      if tag_name
+        doc.method_missing(tag_name, options) { yield(doc) }
+      else
+        yield(doc)
+      end
+    end
+    root.to_html.html_safe
+  end
+end
