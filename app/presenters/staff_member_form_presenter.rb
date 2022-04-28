385c4ac8cbbcceaa20db9a8ba86eb8e753a68baa
yoneyama ryo <uryonym@outlook.com>
2022-04-28 22:36:18 +0900

    15-3-3 フォームプレゼンターの拡張

--- app/presenters/staff_member_form_presenter.rb
+++ app/presenters/staff_member_form_presenter.rb
@@ -1,10 +1,21 @@
 class StaffMemberFormPresenter < FormPresenter
+  def password_field_block(name, label_text, options = {})
+    super(name, label_text, options) if object.new_record?
+  end
+
   def full_name_block(name1, name2, label_text, options = {})
     markup(:div, class: 'input-block') do |m|
       m <<
         label(name1, label_text, class: options[:required] ? 'required' : nil)
       m << text_field(name1, options)
       m << text_field(name2, options)
+    end
+  end
+
+  def suspended_check_box
+    markup(:div, class: 'check-boxes') do |m|
+      m << check_box(:suspended)
+      m << label(:suspended, 'アカウント停止')
     end
   end
 end
