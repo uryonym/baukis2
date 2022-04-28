34b2655ceac6d802a5bd5ede2957630971c70458
yoneyama ryo <uryonym@outlook.com>
2022-04-28 21:59:48 +0900

    15-1-3 delegateによるコードの簡素化

--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -1,12 +1,10 @@
 class StaffMemberPresenter < ModelPresenter
+  delegate :suspended?, to: :object
+
   # 職員の停止フラグのOn/Offを表現する記号を返す
   # On: BALLOT BOX WITH CHECK (U+2611)
   # Off: BALLOT BOX (U+2610)
   def suspended_mark
-    if object.suspended?
-      view_context.raw('&#x2611;')
-    else
-      view_context.raw('$#x2610;')
-    end
+    suspended? ? raw('&#x2611;') : raw('&#x2610;')
   end
 end
