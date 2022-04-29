2c2caaa113858428c3bc130a0af2f5a127cc96f8
yoneyama ryo <uryonym@outlook.com>
2022-04-30 07:34:05 +0900

    16-3 顧客アカウントの新規登録・編集フォームの実装

---
+++ app/presenters/user_form_presenter.rb
@@ -0,0 +1,15 @@
+class UserFormPresenter < FormPresenter
+  def password_field_block(name, label_text, options = {})
+    super(name, label_text, options) if object.new_record?
+  end
+
+  def full_name_block(name1, name2, label_text, options = {})
+    markup(:div, class: 'input-block') do |m|
+      m << decorated_label(name1, label_text, options)
+      m << text_field(name1, options)
+      m << text_field(name2, options)
+      m << error_messages_for(name1)
+      m << error_messages_for(name2)
+    end
+  end
+end
