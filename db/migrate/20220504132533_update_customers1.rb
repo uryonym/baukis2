6f458b5185e8c16616a013d9b119dc24e159e005
yoneyama ryo <uryonym@outlook.com>
2022-05-04 22:31:07 +0900

    ex3-1-3 誕生年、誕生月、誕生日の設定

---
+++ db/migrate/20220504132533_update_customers1.rb
@@ -0,0 +1,21 @@
+class UpdateCustomers1 < ActiveRecord::Migration[6.0]
+  def up
+    execute(
+      '
+      UPDATE customers SET
+        birth_year = EXTRACT(YEAR FROM birthday),
+        birth_month = EXTRACT(MONTH FROM birthday),
+        birth_mday = EXTRACT(DAY FROM birthday)
+        WHERE birthday IS NOT NULL
+    ',
+    )
+  end
+
+  def down
+    execute(
+      '
+      UPDATE customers SET birth_year = NULL, birth_month = NULL, birth_mday = NULL
+    ',
+    )
+  end
+end
